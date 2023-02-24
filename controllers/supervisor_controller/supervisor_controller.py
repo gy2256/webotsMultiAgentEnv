c22a7b54dd7dadda691d647b47c5f831f6be8ae8
Mac <mac@Macs-Laptop-2.lan>
2023-02-24 19:46:29 +0000

    Added controller

--- controllers/supervisor_controller/supervisor_controller.py
+++ controllers/supervisor_controller/supervisor_controller.py
@@ -16,9 +16,21 @@ timestep = 64
 #  motor = robot.getDevice('motorname')
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
-agent0_state_receiver = supervisor_robot.getDevice("supervisor_receiver")
+agent0_state_receiver = supervisor_robot.getDevice("agent0_state_receiver")
 agent0_state_receiver.enable(timestep)
 agent0_state_receiver.setChannel(0)
+
+agent1_state_receiver = supervisor_robot.getDevice("agent1_state_receiver")
+agent1_state_receiver.enable(timestep)
+agent1_state_receiver.setChannel(1)
+
+agent2_state_receiver = supervisor_robot.getDevice("agent2_state_receiver")
+agent2_state_receiver.enable(timestep)
+agent2_state_receiver.setChannel(2)
+
+agent3_state_receiver = supervisor_robot.getDevice("agent3_state_receiver")
+agent3_state_receiver.enable(timestep)
+agent3_state_receiver.setChannel(3)
 
 
 control_agent0_emitter = supervisor_robot.getDevice("control_agent0_emitter")
@@ -39,15 +51,26 @@ control_agent3_emitter.setChannel(3)
 while supervisor_robot.step(timestep) != -1:
 
     if agent0_state_receiver.getQueueLength()>0:
-        received_data = agent0_state_receiver.getString()
+        #Receive state information from the environment
+        received_agent0_data = agent0_state_receiver.getString()
+        received_agent1_data = agent1_state_receiver.getString()
+        received_agent2_data = agent2_state_receiver.getString()
+        received_agent3_data = agent3_state_receiver.getString()
+        
         agent0_state_receiver.nextPacket()
+        agent1_state_receiver.nextPacket()
+        agent2_state_receiver.nextPacket()
+        agent3_state_receiver.nextPacket()
+    
+    #Control Code
+    
     
     # Agent 0 Control 
     agent0_cmd_vel = "[1.0, 0.0, 0]"
     control_agent0_emitter.send(agent0_cmd_vel)
     
     # Agent 1 Control
-    agent1_cmd_vel = "[-1.0, 0.0, 0]"
+    agent1_cmd_vel = "[1.0, 0.0, 0]"
     control_agent1_emitter.send(agent1_cmd_vel)
     
     # Agent 2 Control
